ce0c46faeb0303b1f8d26694807d6c8fa780594d
Apurva Milind Gaikwad <gaikwadapurva65@gmail.com>
2019-12-02 21:18:41 +0530

    Update MainProgram.py

--- MainProgram.py
+++ MainProgram.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import psycopg2
 import itertools
 
@@ -5,10 +7,13 @@ question1 = '1. What are the most popular three articles of all time?'
 ans_query1 = '''SELECT * FROM article_count LIMIT 3;'''
 
 question2 = '2. Who are the most popular article authors of all time?'
-ans_query2 = '''SELECT name, SUM(views) AS views FROM author_count_data GROUP BY name ORDER BY views DESC;'''
+ans_query2 = '''SELECT name, SUM(views) AS views FROM author_count_data
+ GROUP BY name ORDER BY views DESC;'''
 
 question3 = '3. On which days did more than 1% of requests lead to errors?'
-ans_query3 = '''SELECT * FROM percentage WHERE percent > 1.0;'''
+ans_query3 = '''SELECT date, cast(percent as numeric) FROM percentage
+ WHERE percent > 1.0;'''
+
 
 def solution(query):
     db = psycopg2.connect("dbname=news")
@@ -18,18 +23,23 @@ def solution(query):
     db.close()
     return result
 
+
 result1 = solution(ans_query1)
 result2 = solution(ans_query2)
 result3 = solution(ans_query3)
 
+
 def printAns(ans):
     if ans == result1 or ans == result2:
-        for r in range(len(ans)):
-            print(str(ans[r][0]) + " - " + str(ans[r][1]) + " Views")
+        for r in ans:
+            output1 = '{name} - {calc} Views'.format(title=r[0], calc=r[1])
+            print(output1)
+        print("\n")
     else:
-        for r in range(len(ans)):
-            print(str(ans[r][0]) + " - " + str(ans[r][1]) + "% errors")
-    print("\n")
+        for r in ans:
+            output2 = '{date} - {calc}% errors'.format(date=r[0], calc=r[1])
+            print(output2)
+        print("\n")
 
 
 if __name__ == '__main__':
